ae5fa2d6fde6821caf0b546f9fad192c6acc6fed
Dimitar Dimitrov <105887060+Dimpex0@users.noreply.github.com>
2023-08-27 11:16:10 +0300

    code is cleaner

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 function toggle() {
-    const toggleBtn = document.getElementsByClassName('toggle')[0];
-    const list = document.getElementsByClassName('nav-list')[0];
-    const icon = document.getElementsByClassName('nav-icon')[0];
+    const nav = document.getElementsByClassName('nav-bar')[0];
 
-    toggleBtn.classList.toggle('closeBtn');
-    list.classList.toggle('open');
-    if (icon.style.display === 'none') {
-        icon.style.display = 'block';
-    } else {
-        icon.style.display = 'none';
-    }
+    nav.classList.toggle('open');
 }
